329839eb05a77b62f0cb95d3695abdd15770c2e7
Johannes Stark <johannes.stark@valiton.com>
2022-08-24 15:35:13 +0200

    saferpay expects amount to be of type string

--- Utils/SaferpayFormatHelper.php
+++ Utils/SaferpayFormatHelper.php
@@ -13,16 +13,16 @@ class SaferpayFormatHelper
     /**
      * @param $amount
      * @param int $base
-     * @return float
+     * @return string
      */
     public static function formatAmount($amount, $base = 100)
     {
         if ($amount <= 0) {
-            return 0;
+            return '0';
         }
         if ($base <= 0) {
-            return 0;
+            return '0';
         }
-        return $amount * $base;
+        return (string) ($amount * $base);
     }
 }
